9c84e9bbcb50e573c3fd432547cf9eaba63b4318
aman <aman@amt.in>
2024-10-03 13:00:35 +0530

    add project

---
+++ src/main/java/com/example/crud/demo/CrudOpreationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOpreationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOpreationsApplication.class, args);
+	}
+
+}
