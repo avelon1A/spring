c5d81b476cbae00696e32f66057df3470ce6a99c
aman <amanavelon@gmail.com>
2024-10-09 14:42:28 +0530

    home controller

--- src/main/java/com/example/crud/spring/controller/HomeController.java
+++ src/main/java/com/example/crud/spring/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.crud.spring.controller;
 
 import com.example.crud.spring.dto.StudentDto;
+import com.example.crud.spring.entity.Text;
 import com.example.crud.spring.util.parser.StudentParser;
 import com.example.crud.spring.dto.StudentReturnDto;
 import com.example.crud.spring.entity.Student;
@@ -9,21 +10,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+
+
+
 @RestController
 public class HomeController {
 
     private final StudentService studentService;
     private final StudentParser studentParser;
+    private final Text text = new Text();
 
     @Autowired
     public HomeController(StudentService studentService, StudentParser studentParser) {
         this.studentService = studentService;
         this.studentParser = studentParser;
+
     }
 
     @GetMapping("/")
-    public String home() {
-        return "Hello World";
+    public Text home() {
+        return text;
     }
 
     @PostMapping("/saveStudent")
