efcd530f7590e1ee97965efacc76d1153e602924
aman <amanavelon@gmail.com>
2024-10-09 14:42:20 +0530

    learning about pojo

---
+++ src/main/java/com/example/crud/spring/entity/Text.java
@@ -0,0 +1,13 @@
+package com.example.crud.spring.entity;
+
+public class Text {
+    String text = "hello";
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
